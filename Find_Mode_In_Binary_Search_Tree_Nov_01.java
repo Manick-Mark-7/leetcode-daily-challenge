ac86a552901de08e613932e210e872c9ebaa3971
Manickavasagam S <144592367+Manick-Mark-7@users.noreply.github.com>
2023-11-01 13:41:11 +0530

    Find the repeated element in the BST

---
+++ Find_Mode_In_Binary_Search_Tree_Nov_01.java
@@ -0,0 +1,61 @@
+package leetcode.binarySearch.daily_challenge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Find_Mode_In_Binary_Search_Tree_Nov_01 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        private int currentVal;
+        private int currentCount = 0;
+        private int maxCount = 0;
+        private List<Integer> modes = new ArrayList<>();
+
+        public int[] findMode(TreeNode root) {
+            inOrderTraversal(root);
+            int[] result = new int[modes.size()];
+            for (int i = 0; i < modes.size(); i++) {
+                result[i] = modes.get(i);
+            }
+            return result;
+
+        }
+
+        private void inOrderTraversal(TreeNode node) {
+            if (node == null) return;
+
+            inOrderTraversal(node.left);
+
+            currentCount = (node.val == currentVal) ? currentCount + 1 : 1;
+            if (currentCount == maxCount) {
+                modes.add(node.val);
+            } else if (currentCount > maxCount) {
+                maxCount = currentCount;
+                modes.clear();
+                modes.add(node.val);
+            }
+            currentVal = node.val;
+
+            inOrderTraversal(node.right);
+        }
+    }
+}
