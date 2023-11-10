65faa703a8fdf2a2f0ab763acef2598b14e4cdc2
Manickavasagam S <144592367+Manick-Mark-7@users.noreply.github.com>
2023-11-10 19:04:25 +0530

    Create the 1D array from 2D array by adjusting the pairs

---
+++ Restore_The_Array_From_Adjacent_Pair_Oct_10.java
@@ -0,0 +1,44 @@
+package leetcode.binarySearch.daily_challenge;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Restore_The_Array_From_Adjacent_Pair_Oct_10 {
+    public static void main(String[] args) {
+        int[][] vals = {{2,1},{3,4},{3,2}};
+        System.out.println(Arrays.toString(restoreArray(vals)));
+    }
+    public static int[] restoreArray(int[][] vals) {
+        Map<Integer, int[]> pairs = new HashMap<>();
+        for (int i = 0; i < vals.length; i++) {
+            if (pairs.containsKey(vals[i][0])) {
+                pairs.get(vals[i][0])[1] = vals[i][1];
+            } else {
+                pairs.put(vals[i][0], new int[] {vals[i][1], -1000000});
+            }
+            if (pairs.containsKey(vals[i][1])) {
+                pairs.get(vals[i][1])[1] = vals[i][0];
+            } else {
+                pairs.put(vals[i][1], new int[] {vals[i][0], -1000000});
+            }
+        }
+        int[] result = new int[vals.length + 1];
+        int start = -1000000;
+        for (Map.Entry<Integer, int[]> entry : pairs.entrySet()) {
+            if (entry.getValue()[1] == -1000000) {
+                start = entry.getKey();
+            }
+        }
+        result[0] = start;
+        int left = -1000000;
+        for (int i = 1; i < result.length; i++) {
+            int[] val = pairs.get(start);
+            int newval = (val[0] == left ? val[1] : val[0]);
+            result[i] = newval;
+            left = start;
+            start = newval;
+        }
+        return result;
+    }
+}
