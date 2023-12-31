21f17cbe990b8411d9af19383b30d2ae4545a2aa
Manickavasagam S <144592367+Manick-Mark-7@users.noreply.github.com>
2023-12-31 22:01:16 +0530

    Add files via upload

---
+++ Largest_Substring_Between_Two_Equal_Characters_Dec_31.java
@@ -0,0 +1,25 @@
+package leetcode.binarySearch.daily_challenge;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Largest_Substring_Between_Two_Equal_Characters_Dec_31 {
+    public static void main(String[] args) {
+        String str = "abca";
+        int ans = maxLengthBetweenEqualCharacters(str);
+        System.out.println(ans);
+    }
+
+    private static int maxLengthBetweenEqualCharacters(String s) {
+        int ans = -1;
+        Map<Character,Integer> map = new HashMap<>();
+        for(int i = 0;i<s.length();i++){
+            if(map.containsKey(s.charAt(i)))
+                ans = Math.max(ans,i-map.get(s.charAt(i))-1);
+            else
+                map.put(s.charAt(i),i);
+        }
+        return ans;
+
+    }
+}
